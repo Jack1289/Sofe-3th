1664df650cfb7f80e12188bcd8f1ab926c8ecd8b
Josue Rembao <josue.garcia.rembao@gmail.com>
2017-10-07 14:56:50 -0600

    Equipo 6 Josuen Garcia y Hector Robles

--- Java/src/main/java/sofe/exercise1/ContinuousArray.java
+++ Java/src/main/java/sofe/exercise1/ContinuousArray.java
@@ -1,9 +1,35 @@
 package sofe.exercise1;
+        
+import java.util.ArrayList;
 
 public class ContinuousArray {
 
     public static boolean isAlmostIncreasingSequence(int[] sequence) {
-        return null;
+        ArrayList<Integer> temporalList=new ArrayList<Integer>();
+        boolean flag;
+                
+        for (int i = 0; i < sequence.length; i++) {
+            if(i != sequence.length-1){
+                if(sequence[i] < sequence[i+1]){
+                    temporalList.add(sequence[i]);
+                }
+            }else {
+            temporalList.add(sequence[i]);
+            }
+               
+        }
+        /* for (int i : sequence) {
+        //if(i != sequence.length-1){
+        if(sequence[i] < sequence[i+1]){
+        temporalList.add(sequence[i]);
+        }
+        //}
+        
+        }*/
+        if(temporalList.size() == sequence.length-1 || temporalList.size() == sequence.length){ 
+            flag = true; }
+        else{flag = false;}
+        return flag;
     }
 
 }
--- Java/src/main/java/sofe/exercise2/ReverseParenthesis.java
+++ Java/src/main/java/sofe/exercise2/ReverseParenthesis.java
@@ -1,9 +1,47 @@
 package sofe.exercise2;
 
+import java.util.ArrayList;
+
 public class ReverseParenthesis {
 
     public static String reverseParenthesis(String text) {
-        return "";
+        ArrayList<String> composeString = new ArrayList<String>();
+        String result="";
+        char[] charText = text.toCharArray();
+        String temporalSubString="";
+        int i = 0;
+        while (i < text.length()) {
+            if(charText[i]!= '(' && charText[i]!= ')'){
+                temporalSubString = temporalSubString+charText[i];
+            }
+            else if(charText[i] == '('){
+                composeString.add(temporalSubString);
+                temporalSubString="(";
+            }
+            else if(charText[i] == ')'){
+                temporalSubString = temporalSubString.substring(1);
+                temporalSubString = new StringBuilder(temporalSubString).reverse().toString();
+                composeString.add(temporalSubString);
+                int j=i+1;
+                temporalSubString ="";
+                while(j<text.length()){
+                    temporalSubString = temporalSubString+charText[j];
+                    j++;
+                }
+                composeString.add(temporalSubString);
+                text = "";
+                for (int k = 0; k < composeString.size(); k++) {
+                    text += composeString.get(k);
+                }
+                i=text.length();
+            }
+            i++;
+        }
+        
+        if(text.contains("(") || text.contains(")")){
+          result =  reverseParenthesis(text);
+        }else { result =text;}
+        return result;
     }
 
 }
